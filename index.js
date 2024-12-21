cf63f85759ea85276583edca55afb0abe43c2759
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-12-21 11:16:52 +0500

    database connected

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 import express from "express";
 import Router from "./routes/index.js";
+import mongoose from "./db/dbConnect.js";
 
 const app = express();
+
+app.use(express.json());
+
+mongoose.connection.on("open", () => {
+    console.log("Database Connected");
+})
+
+mongoose.connection.on("error", (err) => {
+    console.log("Database Not Connected", err.message);
+})
 
 app.get("/", (req, res) => {
     res.send("Hello World")
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 import express from "express"
+import usersRouter from "./users/index.js";
 
 const Router = express.Router();
 
-UserRouter.use("/users", usersRouter)
+Router.use("/users", usersRouter)
 
 export default Router;
